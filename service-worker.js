5d53a52c4c4f3bcdea3dbefef24a88d719eeba1a
angelocallocallo1111@gmail.com <angelocallocallo1111@gmail.com>
2025-07-09 19:40:36 +0800

    updateaPI

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: ZPDDnrGG */
+/* Manifest version: LN4rZyCW */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
